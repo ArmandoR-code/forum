0fed2d33de8a7848223c62689fcf5565b20c043c
Army <jarupa310@gmail.com>
2021-09-29 11:41:15 -0500

    Correccion de errores de seguridad

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '/myproject/secret_key.txt'
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
