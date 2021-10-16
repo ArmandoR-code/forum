d533368a691f73a4dce4b737a71f0b2e3f93ec23
Army <jarupa310@gmail.com>
2021-10-16 14:42:03 -0500

    Se realizaron correciones en view.py y settings.py

--- boards/views.py
+++ boards/views.py
@@ -1,24 +1,8 @@
-from django.http import HttpResponse
-from .models import Board
 from django.shortcuts import render
+
+from .models import Board
+
 
 def home(request):
     boards = Board.objects.all()
-    boards_names = list()
-
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-
-    return HttpResponse(response_html)
-
-
-#Create your views here.
-
-def home(request):
-    """ Atiende la perición GET / """ 
-    return render(request, "boards/home.html")
-    
-
-
+    return render(request, 'home.html', {'boards': boards})
--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
